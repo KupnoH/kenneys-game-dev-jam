7822f7b923a9a6de80130805debe4bace82078e1
SeyidAhmetYogurtcu <SeyidAhmetYogurtcu@gmail.com>
2023-07-22 07:23:58 +0300

    UPDATE
    Cinemachine cameras  added and modified.
    Ship Atack, movement, turn,idle animation added.
    Get out from ship is mostly done.(bug exist).

--- Assets/Scripts/AttackController.cs
+++ Assets/Scripts/AttackController.cs
@@ -4,8 +4,12 @@ using UnityEngine;
 
 public class CannonController : MonoBehaviour
 {
+    public float projectileSpeed = 50f;
+    // ReSharper disable Unity.PerformanceAnalysis
     public void Fire(GameObject cannonPrefab, Transform cannonSpawnPoint)
     {
-        Instantiate(cannonPrefab, cannonSpawnPoint.position, cannonSpawnPoint.rotation);
+        GameObject projectile =  Instantiate(cannonPrefab, cannonSpawnPoint.position, cannonSpawnPoint.rotation);
+        Rigidbody rb = projectile.GetComponent<Rigidbody>();
+        rb.velocity = cannonSpawnPoint.forward * projectileSpeed;
     }
 }
--- Assets/Scripts/ShipController.cs
+++ Assets/Scripts/ShipController.cs
@@ -1,35 +1,46 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class ShipController : MonoBehaviour
 {
     public float speed = 5f;
-    public float swayAmount = 0.5f;
+    public float rotationSpeed = 40;
+    public float horizontalSwayAmount = 1f;
+    public float verticalwayAmount = 2f;
     public GameObject cannonPrefab;
     public Transform cannonSpawnPoint;
     public GameObject playerPrefab;
     public Transform playerSpawnPoint;
 
     private bool isPlayerOnShip = true;
-    private PlayerController playerController;
+    public PlayerManager playerManager;
     private CannonController cannonController;
-
+    public float maxDistance = 10f;
+    public LayerMask islandLayer;
     void Start()
     {
-        playerController = GetComponent<PlayerController>();
         cannonController = GetComponent<CannonController>();
     }
+
 
     void Update()
     {
         // Ship movement
         float horizontalInput = Input.GetAxis("Horizontal");
         float verticalInput = Input.GetAxis("Vertical");
-        transform.position += new Vector3(horizontalInput, 0, verticalInput) * (speed * Time.deltaTime);
+        transform.Translate(new Vector3(horizontalInput, 0, verticalInput) * (speed * Time.deltaTime));
 
-        // Ship swaying
-        transform.rotation = Quaternion.Euler(0, 0, Mathf.Sin(Time.time) * swayAmount);
+        // Ship rotation
+        if (Input.GetKey(KeyCode.Q))
+        {
+            gameObject.transform.Rotate(Vector3.up, -rotationSpeed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.E))
+        {
+            gameObject.transform.Rotate(Vector3.up, rotationSpeed * Time.deltaTime);
+        }
+
+        //Ship swaying
+        //SwayShip();
 
         // Cannon attack
         if (Input.GetKeyDown(KeyCode.Space))
@@ -38,14 +49,41 @@ public class ShipController : MonoBehaviour
         }
 
         // Player disembark
-        if (Input.GetKeyDown(KeyCode.F))
+        //if (Input.GetKeyDown(KeyCode.F))
+        //{
+        //    if (isPlayerOnShip)
+        //    {
+        //        RaycastHit hit;
+        //        if (Physics.SphereCast(transform.position, maxDistance, Vector3.forward, out hit, 1, islandLayer))
+        //        {
+
+        //        }
+        //    }
+        //}
+    }
+    //private void SwayShip()
+    //{
+    //    m_Ship.transform.rotation = Quaternion.Euler(Mathf.Cos(Time.time) * verticalwayAmount, 0, Mathf.Sin(Time.time) * horizontalSwayAmount);
+    //}
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.CompareTag("Land"))
         {
-            if (isPlayerOnShip)
+            if (Input.GetKeyDown(KeyCode.F) && isPlayerOnShip)
             {
-                playerController.Disembark(playerPrefab, playerSpawnPoint);
+                Vector3 distance = other.ClosestPoint(transform.position);
+                playerManager.Disembark(playerPrefab, playerSpawnPoint, distance);
                 isPlayerOnShip = false;
             }
         }
     }
+    void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, maxDistance);
+    }
 }
+
+
 
